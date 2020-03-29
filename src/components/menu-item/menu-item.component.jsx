9bc7f4aed86e9a88925bc961753930db5a820291
Kayla <kbaistrocchi@gmail.com>
2020-03-29 16:01:20 -0400

    style: add hover effect to menu items

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imgUrl, size }) => (
-    // React passes a property called 'style' to each html tag
-    // style can take an object value that can take props
-    <div 
-    style={{
-        backgroundImage: `url(${imgUrl})`
-        // now, if our img url changes then our style changes
-        // to make larger, can add a size prop that won't get used if no size prop exists
-    }}
+    
+
     // can add the size as a class name passed from props
-    className={`menu-item ${size}`}>
-        <div className='content'>
-            <h1 className='title'>{title}</h1>
+    <div className={`menu-item ${size}`}>
+        <div className='background-image'
+        // React passes a property called 'style' to each html tag
+        // style can take an object value that can take props
+        style={{
+            backgroundImage: `url(${imgUrl})`
+            // now, if our img url changes then our style changes
+            // to make larger, can add a size prop that won't get used if no size prop exists
+        }}></div>
+        
+        <div className='content'>  
+            {/* seeing as {title is JS, we can use any JS methods we want on it} */}
+            <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
