a8f9900e345416eea8ec4309c4eb66830fc97238
Kayla <kbaistrocchi@gmail.com>
2020-03-29 19:24:20 -0400

    feat: wrap Homepage with Switch and Route - incl. comments to explain

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
 
+const SomePage = () => (
+    <div>
+      <h1>Some Page</h1>
+    </div>
+)
+
 function App() {
   return (
     <div>
-      Stuff
-      <Homepage />
+      App
+      {/* Instead of rendering the component, we render the Route */}
+      {/* Route will then point to the component which will be a page */}
+      {/* we wrap all the Route components in <Switch> so that only one will show at a time */}
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/somepath' component={SomePage} />
+        {/* Route provides access to 3 important properties:  match, history and location
+            BUT, only to its FIRST child, not to their children
+            In this case, Homepage and HatsPage, but not MenuItem or Directory, etc
+            In order for 'grandchildren' to get these props, we can use the 
+            withRouter from react-router-dom on the grandchild component
+        */}
+      </Switch>
+    
     </div>
   );
 }
