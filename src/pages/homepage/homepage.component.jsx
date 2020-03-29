4df5a936519be55a686a49b33132ccd148f2427a
Kayla <kbaistrocchi@gmail.com>
2020-03-28 21:25:08 -0400

    feat: replace directory with the directory component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+import Directory from '../../components/directory-menu/directory.component';
+
+const Homepage = () => (
+    <div className='homepage'>
+        <Directory />
+    </div>
+);
+
+export default Homepage;
