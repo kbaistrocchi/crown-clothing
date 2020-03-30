ebb9d8aa84abd0b613d14cc56f8b4b74262fb06c
Kayla <kbaistrocchi@gmail.com>
2020-03-30 15:45:05 -0400

    style: sign-in title margins and display flex

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -32,7 +32,7 @@ class SignIn extends React.Component {
     render() {
         return (
             <div className='sign-in'>
-                <h2>I already have an account</h2>
+                <h2 className='title'>I already have an account</h2>
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
@@ -55,7 +55,7 @@ class SignIn extends React.Component {
                     />
 
                     <CustomButton type='submit' >
-                        Submit Form
+                        Sign In
                     </ CustomButton>
                 </form>
             </div>
