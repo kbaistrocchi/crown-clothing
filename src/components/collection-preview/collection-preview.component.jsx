9a52ad4e96ee68f2346859b8ff14c2e91b9328ca
Kayla <kbaistrocchi@gmail.com>
2020-03-29 21:01:36 -0400

    note: warning comment about large arrays in render function

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -10,6 +10,8 @@ const CollectionPreview = ({title, items}) => (
                 items
                 .filter((item, index) => index < 4)
                 .map((item) => <div key={item.id}>{item.name}</div>)
+                // NOTE this will be called everytime CollectionPreview is re-rendered
+                // and this could become slow if the array is really large
             }
         </div>
     </div>
