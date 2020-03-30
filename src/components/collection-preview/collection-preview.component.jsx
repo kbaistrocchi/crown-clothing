e256b18e4c71f4ed180395e4f2514dae847e2a94
Kayla <kbaistrocchi@gmail.com>
2020-03-29 20:59:27 -0400

    feat: filter items array so that only 4 appear in collection preview

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -7,7 +7,9 @@ const CollectionPreview = ({title, items}) => (
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preivew'>
             {
-                items.map((item) => <div key={item.id}>{item.name}</div>)
+                items
+                .filter((item, index) => index < 4)
+                .map((item) => <div key={item.id}>{item.name}</div>)
             }
         </div>
     </div>
