a1c63b98fd64c701c36e542c134186453ae0538d
Kayla <kbaistrocchi@gmail.com>
2020-03-30 14:19:14 -0400

    feat: handleChange method updates state dynamically by de-structuring  event.target

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -17,6 +17,15 @@ class SignIn extends React.Component {
         this.setState({'email': '', 'password': ''});
     }
 
+    handleChange = (e) => {
+        // deconstruct to pull value and name off of e.target
+        const { value, name } = e.target;
+        // then, dynamically setState using those vars (ex: name will be either 'email' or 'password')
+            // I think the [] are used in case it contains a space - it's safer
+        this.setState({ [name]: value });
+        // this allows us to pass the same function into both inputs below
+    }
+ 
     render() {
         return (
             <div className='sign-in'>
@@ -24,11 +33,25 @@ class SignIn extends React.Component {
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input name='email' type='email' value={this.state.email} required />
+                    <input 
+                        name='email' 
+                        type='email' 
+                        value={this.state.email} 
+                        onChange={this.handleChange}
+                        required 
+                    />
                     <label>Email</label>
-                    <input name='password' type='password' value={this.state.password} required />
+
+                    <input 
+                        name='password' 
+                        type='password' 
+                        value={this.state.password} 
+                        onChange={this.handleChange}
+                        required 
+                    />
                     <label>Password</label>
-                    <input type='submit'/>
+
+                    <input type='submit' value='Submit Form' />
                 </form>
             </div>
         )
