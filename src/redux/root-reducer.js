7034606a794ba09252c8b65e1fead3edbb1438c0
Kayla <kbaistrocchi@gmail.com>
2020-04-01 14:14:58 -0400

    feat: create cart reducer/actions/types and toggle state for hide/show

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import userReducer from './user/user.reducer';
+import cartReducer from './cart/cart.reducer';
 
 // combineReducer just takes all the reducers and combines them into one
 
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer
 });
