5ddd3651cc7df67c5b36e11ae5647a4b3dae7ef0
Kayla <kbaistrocchi@gmail.com>
2020-04-01 19:23:04 -0400

    feat: create addItem action and associated redux code

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl })=> (
+const CollectionItem = ({ id, name, price, imageUrl, addItem })=> (
     <div className='collection-item'>
         <div 
         className='image'
@@ -26,4 +28,12 @@ const CollectionItem = ({ id, name, price, imageUrl })=> (
     </div>
 )
 
-export default CollectionItem;
+// whenever there's an addItem above, get the addItem function and pass in the item as the payload 
+// so once mapDispatchtoProps is defines we'll have access to the addItem property (as a prop)
+// and therefore need to pass it into the functional component (as a prop)
+
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
