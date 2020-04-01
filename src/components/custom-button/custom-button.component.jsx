5487f0cbde4e560ae6213ed81e48a1812aaad390
Kayla <kbaistrocchi@gmail.com>
2020-04-01 18:44:39 -0400

    feat: dynamically add 'inverted' className to CustomButton

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -4,9 +4,15 @@ import './custom-button.styles.scss';
 
 // this will be a Presentational component (same as stateless functional comp)
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-    <button className={`custom-button ${isGoogleSignIn ? 'google-sign-in' : ''}`} {...otherProps}>  
-            {/* otherProps could include type='submit' for example */}
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
+    <button 
+        className={`custom-button 
+                    ${isGoogleSignIn ? 'google-sign-in' : ''}
+                    ${inverted ? 'inverted' : ''}
+                    `} 
+        {...otherProps}>  
+        {/* otherProps could include type='submit' for example */}
+        
         {children}
             {/* children will be whatever is between the <CustomButton> tags
             i.e. the text we want on the button */}
