d69f5d59609a82692c906ff4b82eb7ee02586f94
Kayla <kbaistrocchi@gmail.com>
2020-03-31 22:01:01 -0400

    feat: Redux - dispatch action to update state for Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 
 import './App.scss';
@@ -9,22 +10,19 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sing-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
 // we want to store info on whether or not a user is logged in. 
 // So we need state, which means changing comp into a class comp
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    // we're using this method more than once so we can de-structure it
+    const { setCurrentUser } = this.props;
+
     // we use a method from the auth library
     // take function as param, and its param is the user state
     // then is sets ours state with that status
@@ -35,16 +33,16 @@ class App extends React.Component {
         const userRef = await createUserProfileDocument(userAuth);
         // for details on how this works, see video #91
         userRef.onSnapshot(snapShot => { 
-          this.setState({
-            currentUser: {
+          // we can replace setState with our action and pass it the object that we were 
+          // previously using to set currentUser
+          setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            }
-        }, () => console.log(this.state));
+            })
       });
     }
       // if userAuth is null, then set currentUser to null
-      this.setState({ currentUser: userAuth });
+      setCurrentUser(userAuth);
       // this is an open subscription - always checking status even though it's only called ONCE
       //  and therefore needs to be closed  when the component is unmounted
       // we can do this by calling it again in another lifecycle method
@@ -80,4 +78,11 @@ class App extends React.Component {
   
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+// dipatch() takes whatever is passed into it and knows that it's an action
+//  and sends it to every reducer
+
+
+export default connect(null, mapDispatchToProps)(App);
