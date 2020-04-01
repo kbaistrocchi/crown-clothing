82edd621aa1a53ef5dea0debe8ee0236293b09e0
Kayla <kbaistrocchi@gmail.com>
2020-04-01 19:48:01 -0400

    feat: refactor to handle onClick and add entire item to cartItem array

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -6,27 +6,43 @@ import { addItem } from '../../redux/cart/cart.actions';
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl, addItem })=> (
-    <div className='collection-item'>
-        <div 
-        className='image'
-        style={{
-            backgroundImage: `url(${imageUrl})`
-        }}
-        />
-        <div className='collection-footer'>
-            <span className='name'>{name}</span>
-            <span className='price'>${price}</span>
+// Because we need access to the entire item, we modified how it was
+// passed in from CollectionPreview and we need to rewrite some stuff
+
+// const CollectionItem = ({ id, name, price, imageUrl, addItem })=> (
+    // We refactor the line above to adjust for change in props from parent, 
+    // now being passed as 'item' rather than individual pieces. We now 
+    // wrap the entire component in {} which let's it know that multiple
+    // pieces of JS will be within (explicity return of the function)
+
+const CollectionItem = ({ item, addItem }) => {
+    const { name, price, imageUrl } = item;
+    return (
+        <div className='collection-item'>
+            <div 
+            className='image'
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+            />
+            <div className='collection-footer'>
+                <span className='name'>{name}</span>
+                <span className='price'>${price}</span>
+            </div>
+
+            <CustomButton inverted onClick={() => addItem(item)}>
+                Add to cart
+            </CustomButton>
+            {/* We can style only these CustomButtons not only by passing props to be
+                classNames, but also by nesting the comp's className ('custom-button')
+                under it's parent div, as usually in Sass  */}
+
         </div>
+    )
+}
+    
+    
 
-        <CustomButton inverted>Add to cart</CustomButton>
-        {/* We can style only these CustomButtons not only by passing props to be
-             classNames, but also by nesting the comp's className ('custom-button')
-             under it's parent div, as usually in Sass  */}
-
-        
-    </div>
-)
 
 // whenever there's an addItem above, get the addItem function and pass in the item as the payload 
 // so once mapDispatchtoProps is defines we'll have access to the addItem property (as a prop)
