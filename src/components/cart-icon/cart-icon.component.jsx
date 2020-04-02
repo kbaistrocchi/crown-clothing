0c07fcb21d73aa8e671c8cb481e322c16fef127d
Kayla <kbaistrocchi@gmail.com>
2020-04-01 22:16:38 -0400

    feat: add reselect library and create cart selectors for itemQuantity in cart-icon
    create a memoized function so that the mapState function does not cause the entire component to re-render if nothing has changed in that little slice of state

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 // import an svg
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-cart-bag.svg';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import './cart-icon.styles.scss';
 
@@ -12,15 +13,18 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => (
         <ShoppingIcon className='shopping-icon' />
         <span className='item-count'>{itemCount}</span>
     </div>
-)
+);
 
+
+// Memoize This
 // In order to have a count in the shopping cart, we need access to the 
 // CartItems state, in particular, the quantity property
 // therefore, we need to map state to props so we have access to that state
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-    itemCount: cartItems.reduce((accumulator, cartItem) => accumulator + cartItem.quantity, 0)
-
-})
+const mapStateToProps = (state) => ({
+    // itemCount: cartItems.reduce((accumulator, cartItem) => accumulator + cartItem.quantity, 0)
+    // Now we re-write this using the cart selector and pass in the entire state
+    itemCount: selectCartItemsCount(state)
+});
 
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,31 @@
+// here we store reusable selectors in a more efficient way
+
+import { createSelector } from 'reselect';
+
+// 1. we make an INPUT SELECTOR. This is a function takes in the whole state
+//  and returns a slice of it. In this case, the cart slice
+const selectCart = state => state.cart;
+
+// 2. now we want a property on the above selector - cartItems
+//  and we make this using the createSelector
+
+// createSelector takes 2 args
+// first - a collection (an array) of input selectors (i.e. from step 1) or created selectors
+// second - a function that returns the value we want from the selector
+//          it's arguments will be the output of the input selectors from first arg
+//           in this case, we want cartItems 
+export const selectCartItems = createSelector(
+    // first argument - input selector(s)
+    [selectCart],
+    // second argument - function that returns
+    cart => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+    // first argument - input selector(s), which was created above
+    [selectCartItems],
+    cartItems => cartItems.reduce(
+        (accumulator, cartItem) => accumulator + cartItem.quantity,
+        0
+        )
+);
