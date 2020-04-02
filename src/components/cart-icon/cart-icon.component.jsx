b22419d050a6a880b6f99bfe21e41851e8ba6938
Kayla <kbaistrocchi@gmail.com>
2020-04-01 22:07:41 -0400

    feat: add item count to shopping cart icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,15 +7,23 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 )
 
-const mapDispatchToProps = dispatch => ({
-    toggleCartHidden: () => dispatch(toggleCartHidden())
+// In order to have a count in the shopping cart, we need access to the 
+// CartItems state, in particular, the quantity property
+// therefore, we need to map state to props so we have access to that state
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    itemCount: cartItems.reduce((accumulator, cartItem) => accumulator + cartItem.quantity, 0)
+
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapDispatchToProps = dispatch => ({
+    toggleCartHidden: () => dispatch(toggleCartHidden())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
