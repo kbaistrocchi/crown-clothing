5878521f1b4cb8ff6139f511fa33b4b1a0978e95
Kayla <kbaistrocchi@gmail.com>
2020-04-01 20:39:54 -0400

    feat: add to cart utility function for use in reducer
    checks that item isn't already in cart, if it is, increase quantity rather than add it again

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,42 @@
+// utility function to help keep code clean
+// these can be used in many places
+
+// Instead of adding the same item to the cart multiple times, 
+// we will check to see if it already exists in the cart and if so,
+// we increase a quantitiy property,
+// if it doesn't exist, we simply add it to the cartItems array
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    // first see if the cartItemToAdd is alerady in the cart
+    const existingCartItem = cartItems.find(cartItem => 
+        cartItem.id === cartItemToAdd.id);  
+        //  returns undefined if not found
+        //  returns the cartItem if is found
+
+    // if it is found then return a NEW object with the cartItem already there
+    // and add a quantity property and increase it by 1 
+        //  needs to be a new object so that state is updated and comps re-render 
+    if(existingCartItem) {
+        return (
+            // map returns a NEW array
+            cartItems.map(cartItem => 
+                // check if the item in the cart is same as item coming in
+                cartItem.id === cartItemToAdd.id ?
+                // if true, return an object with quantity
+                { ...cartItem, quantity: cartItem.quantity + 1 } :
+                // if false, just return the cartItem
+                cartItem 
+                )
+        )
+    }
+
+    // if incoming item was not found in cartItems then we're going to create a NEW
+    // array which contains all the items already in the array (...) 
+    // and add an object containing the new item and give it a quantity property with a base value of 1
+    // i.e. All new items going into the cartItems will be objects with quantity property of 1
+    else {
+        return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
+        // remember, cartItemToAdd is an item and is an object. 
+        // the ...cartItemToAdd passes in all the properties of that 
+        // item object and adds the quantity property
+    }
+}
