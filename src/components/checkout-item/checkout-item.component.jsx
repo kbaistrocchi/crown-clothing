e23a0d797d3ce6181fe9b6a49c332d04a82e3aca
Kayla <kbaistrocchi@gmail.com>
2020-04-06 18:02:56 -0400

    note: about de-structuring props more than 1 layer deep

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 import './checkout-item.styles.scss';
 
 const CheckoutItem = ({ cartItem: { name, price, imageUrl, quantity }}) => (
+    // by de-structuring props like this, we don't have access to cartItem,
+    // only to it's properties that we further de-structured
+
+    
     <div className='checkout-item'>
         <div className='image-container'>
             <img alt='item' src={imageUrl} />
