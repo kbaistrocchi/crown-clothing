ad4ab863af9edf04c446e47bf3689b76aa4d8074
Kayla <kbaistrocchi@gmail.com>
2020-04-06 18:14:11 -0400

    feat: write clearItem action and refactor CheckoutItem to use new action

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { clearItemFromCart } from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem: { name, price, imageUrl, quantity }}) => (
-    // by de-structuring props like this, we don't have access to cartItem,
-    // only to it's properties that we further de-structured
+const CheckoutItem = ({ cartItem, clearItem }) => {
+    const { name, price, imageUrl, quantity } = cartItem;
 
-    
-    <div className='checkout-item'>
-        <div className='image-container'>
-            <img alt='item' src={imageUrl} />
+    return (
+        <div className='checkout-item'>
+            <div className='image-container'>
+                <img alt='item' src={imageUrl} />
+            </div>
+            <span className='name'>{name}</span>
+            <span className='quantity'>{quantity}</span>
+            <span className='price'>{price}</span>
+            <div className='remove-button' onClick={() => clearItem(cartItem)}>
+                &#10005;
+            </div>
+
         </div>
-        <span className='name'>{name}</span>
-        <span className='quantity'>{quantity}</span>
-        <span className='price'>{price}</span>
-        <div className='remove-button'>&#10005;</div>
+    )  
+};
 
-    </div>
-);
+const mapDispatchToProps = dispatch => ({
+    clearItem: item => dispatch(clearItemFromCart(item))
+})
 
-export default CheckoutItem;
+export default connect(null, mapDispatchToProps)(CheckoutItem);
