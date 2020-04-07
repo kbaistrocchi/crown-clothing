c6123d3469d7a745e8752cd7932655cd5e750d0a
Kayla <kbaistrocchi@gmail.com>
2020-04-06 20:22:32 -0400

    feat: add react-persist and configure
    allows us to access window.storage to store state from cart reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,25 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+// type of storage we want from redux-persist. In this case, we want to use the window.storage
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
+// object containing json configurations that we want for persist
+const persistConfig = {
+    // key tells it at what point we want to start storing
+    key: 'root',
+    storage,    // this notation is the same as storage: storage (imported above)
+    // an array of reducers we want to persist
+    whitelist: ['cart']
+}
+
 // combineReducer just takes all the reducers and combines them into one
 
-export default combineReducers({
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
