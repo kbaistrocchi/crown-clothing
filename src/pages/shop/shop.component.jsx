b1cb1c9396759b7e21f3ce301c207dc14e238700
Kayla <kbaistrocchi@gmail.com>
2020-04-07 21:33:08 -0400

    feat: create collection-overview to hold collection-preview

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-import { selectShopCollections } from '../../redux/shop/shop.selectors';
 
-const ShopPage = ({ collections }) => (
+const ShopPage = () => (
     <div className='shop-page'> 
-    {console.log(collections)}
-    {
-        collections.map(({id, ...otherCollectionProps}) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-        ))
-    }
+        <CollectionsOverview /> 
     </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectShopCollections
-})
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
