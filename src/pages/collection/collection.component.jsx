ae23e05d6bf3529da2ca61e524f4e73897ed289b
Kayla <kbaistrocchi@gmail.com>
2020-04-10 19:32:58 -0400

    feat: create Collection comp and nest in a Route on ShopPage with dynamic url

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ match }) => {
+    console.log('match', match);
+    return (
+        <div className='collection'>
+        <h2>Collection PAGE</h2>
+    </div>
+    )
+    
+};
+
+export default CollectionPage;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-
-const ShopPage = () => (
+// the 'match' props is passed down from Route, which ShopPage is nested in
+const ShopPage = ({ match }) => (
     <div className='shop-page'> 
-        <CollectionsOverview /> 
+        {/* we want the CollectionsOverview to display the 
+        current path that we're on (i.e /shop) because the component 
+        doesn't link to a new page.
+        So we use string interpolation to get property off the props
+        that's passed from Route of ShopPage */}
+        <Route exact path={`${match.path}`} component={CollectionsOverview} /> 
+
+        {/* now we nest our Route. Our path will be determined by the 
+            component that it handles (in this case, CollectionPage). The ':' chains 
+            the parameter (from CollectionPage) onto the path  */}
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
 );
 
