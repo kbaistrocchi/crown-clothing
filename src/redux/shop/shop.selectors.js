a2ab7aac1519a26b6a993b2b8135a9d3d93f1d14
Kayla <kbaistrocchi@gmail.com>
2020-04-10 20:05:24 -0400

    feat: create collection selector which takes in parameter (from url)

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,9 +1,30 @@
 import { createSelector } from 'reselect';
 
+// create an object that maps the id to the string corresponding to 
+// the collection name. This is so that the url param gets the 
+// proper collection
+const COLLECTION_ID_MAP = {
+    hats: 1,
+    sneakers: 2,
+    jackets: 3,
+    womens: 4,
+    mens: 5
+}
+
 const selectShop = state => state.shop;
 
 export const selectShopCollections =  createSelector(
     [selectShop],
     (shop) => shop.collections
+);
+
+
+// the collection will be a function which takes in the CollectionID as an arg
+export const selectCollection = collectionsUrlParam => 
+createSelector(
+    [selectShopCollections],
+    collections =>  collections.find(
+        collection => collection.id === COLLECTION_ID_MAP[collectionsUrlParam]
+    )
 );
 
