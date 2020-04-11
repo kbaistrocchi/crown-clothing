8422c754f0d53bf263a4baaf7fbe9687373f60f5
Kayla <kbaistrocchi@gmail.com>
2020-04-11 17:32:38 -0400

    feat: replace collections selector with new one that deals with object

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -4,7 +4,7 @@ import { createStructuredSelector } from 'reselect';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-import { selectShopCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 import './collections-overview.styles.scss';
 
@@ -25,7 +25,7 @@ const CollectionsOverview = ({ collections }) => (
 // in the shop.selector file
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectShopCollections
+    collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionsOverview);
