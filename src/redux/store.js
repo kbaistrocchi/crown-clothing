4588a16c111e0bc276811b44dfc1c91f80d4938c
Kayla <kbaistrocchi@gmail.com>
2020-04-13 14:43:36 -0400

    feat: remove middleware logger from production and test builds

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,12 @@ import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];   // can add more middleware to array in future
+const middlewares = [];   // can add more middleware to array in future
+
+// only use middlewares in development
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
