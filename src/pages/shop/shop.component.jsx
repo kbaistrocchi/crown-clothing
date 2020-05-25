0d867a4cb8c07cc06519382c78439460c88cfebc
Kayla <kbaistrocchi@gmail.com>
2020-05-25 14:32:44 -0400

    feat: user WithSpinner HOC to show spinner while data is being fetched from server
    HOC is rendered by Route using the render function rather than component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,13 +2,29 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CollectionPage from '../collection/collection.component';
-
 import { updateCollections } from '../../redux/shop/shop.actions';
 
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
+// In order to use WithSpinner, a HOC, we will place it where we would normally
+// place the component that it takes in as an argument
+// on the ShopPage, that will be the <Route>s. 
+// So we creat a new component = to the return of the HOC
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
+
 class ShopPage extends React.Component {
+
+    // if we don't need constructor we can just declare a sate
+    //  we need this state for the WithSpinner HOC
+    state = {
+        loading: true
+    };
 
     // whenever using a subscription function, we need to remember to unsubscribe as well
     unsubscribeFromSnapshot = null; 
@@ -23,13 +39,17 @@ class ShopPage extends React.Component {
         //              we GET the data. It's a never-ending stream, which is why
         //              we need to unsubscribe.
 
-    collectionRef.onSnapshot(async snapshot => {
-        console.log('snapshot', snapshot);
-        // in order to shape the data the way we want and add info we may need in this
-        // particular web app, we will write a function in firebase.utils.js
-        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-        console.log(collectionsMap);
-        updateCollections(collectionsMap);
+        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+            console.log('snapshot', snapshot);
+            // in order to shape the data the way we want and add info we may need in this
+            // particular web app, we will write a function in firebase.utils.js
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+            console.log(collectionsMap);
+            updateCollections(collectionsMap);
+            // after the updateCollections is called we know that
+            // the app must have the data and therefore is no longer loading
+            // so we can change the loading state 
+            this.setState({ loading: false });
     })
     }
 
@@ -37,6 +57,7 @@ class ShopPage extends React.Component {
     render() {
         // the 'match' props is passed down from Route, which ShopPage is nested within
         const { match } = this.props;
+        const { loading } = this.state;
         return(
             <div className='shop-page'> 
                 {/* we want the CollectionsOverview to display the 
@@ -44,12 +65,24 @@ class ShopPage extends React.Component {
                 doesn't link to a new page.
                 So we use string interpolation to get property off the props
                 that's passed from Route of ShopPage */}
-                <Route exact path={`${match.path}`} component={CollectionsOverview} /> 
+                {/* <Route exact path={`${match.path}`} component={CollectionsOverview} /> 
+                 instead of using component as in above line, we use render
+                    which takes in arguments which are then passes as the props to 
+                    the component that it's rendering */}
+                    <Route
+                        exact
+                        path={`${match.path}`}
+                        render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />} 
+                    />
         
                 {/* now we nest our Route. Our path uses the ':' to create a chains 
                     parameter called collectionId (or whatever we want) and then gives it
                     the value of whatever is typed (ex shop/couldBeAnything)  */}
-                <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+                <Route
+                    path={`${match.path}/:collectionId`}
+                    render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} />}
+
+                />
             </div>
         );
     } 
